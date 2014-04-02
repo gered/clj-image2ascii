4ba01369d585178ac83ad147783f4be46705a1e7
gered <gered@blarg.ca>
2014-04-02 18:28:54 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# clj-image2ascii
+
+Tools for turning images into ASCII equivalents.
+
+## Usage
+
+FIXME
+
+## License
+
+Distributed under the the MIT License. See LICENSE for more details.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clj-image2ascii "0.1.0-SNAPSHOT"
+  :description "Tools for turning images into ASCII equivalents."
+  :url "https://github.com/gered/clj-image2ascii"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :source-paths      ["src/clojure"]
+  :java-source-paths ["src/java"])
